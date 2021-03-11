6647d18e90442ac136c50d9c3e18bc2e3afc6aab
Chen Hairong <hairong.chen@capgemini.com>
2021-03-11 17:17:30 +0800

    initialize

---
+++ project-with-secrets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+
+namespace project_with_secrets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = ConfigurationManager.AppSettings["password"];
+            Console.WriteLine($"Password is {password}");
+            Console.ReadLine();
+        }
+    }
+}
